9cb306f6ff3fba15ed366279107a2a51b6bb3b33
Adam Weissman <adam.w.weissman@gmail.com>
2025-02-12 22:30:42 +1030

    completed recipe data exploration notebook

--- data/getdata.py
+++ data/getdata.py
@@ -10,15 +10,15 @@ import os
 
 data_dir = "data"
 data_file = "recipe-ingredients.zip"
-download_url = "https://www.kaggle.com/api/v1/datasets/download/kaggle/recipe-ingredients-dataset"
+download_url = "https://www.kaggle.com/api/v1/datasets/download/hugodarwood/epirecipes"
 
 try:
     # Attempt to open the file, if file exists, skip download and unzipping
-    with open(data_dir+'/'+data_file, 'r') as file:
+    with open(os.path.dirname(os.getcwd())+'\\'+data_dir+'\\'+data_file, 'r') as file:
       pass
 except FileNotFoundError:
     # If open file failed, then file wasn't found, so download and unzip
-    os.chdir(data_dir)
+    os.chdir(os.path.dirname(os.getcwd())+'\\'+data_dir)
     url.urlretrieve(download_url, data_file)
     with zp.ZipFile(data_file) as zip_ref:
         zip_ref.extractall()
