55d91c98bc12db64a2df4361e3ebd307a90070ef
awwwwwwww <40839622+awwwwwwww@users.noreply.github.com>
2025-06-24 19:42:00 +0930

    fixinng data path again

--- src/training.py
+++ src/training.py
@@ -15,7 +15,7 @@ if torch.cuda.is_available():
 
 torch.set_default_device(device)
 print(f"Using device = {torch.get_default_device()}")
-data_path = './data/full_format_recipes.json' #path to the dataset
+data_path = 'data/full_format_recipes.json' #path to the dataset
 
 import string
 import unicodedata
